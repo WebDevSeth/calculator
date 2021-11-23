80ad34288f92926557b8f389aa5689f7e73c8c6e
littlezethstar <littlezethstar@gmail.com>
2021-11-23 14:59:02 +0200

    calculator

---
+++ hey.js
@@ -0,0 +1,21 @@
+let sum = "";
+
+function addToSum(operator) {
+    sum = sum + operator;
+    document.getElementById("output").innerText = sum;
+}
+
+function evaluateSum() {
+    document.getElementById("output").innerText = eval(sum);
+}
+
+function clearSum() {
+    sum = "";
+    document.getElementById("output").innerText = sum;
+
+}
+
+function backspace() {
+    sum = sum.slice(0, -1);
+    document.getElementById("output").innerText = sum;
+}
